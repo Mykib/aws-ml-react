eee3d11db2a33c4f89955e2994871a63cb00f381
Mykib <m.brims93@gmail.com>
2021-07-07 04:10:15 +1000

    Added AWS deploy script

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          VSauce, Michael Here.
+          Hi, VSauce, Michael Here.
         </p>
         <a
           className="App-link"
